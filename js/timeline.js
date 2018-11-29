78c118ee964860647c432403d76980d0ce8091be
Your Name <you@example.com>
2018-11-29 16:54:08 -0400

    logica de post, body limitados, campo del autor agregado

--- js/timeline.js
+++ js/timeline.js
@@ -14,15 +14,24 @@ $(document).ready(function () {
             console.log(data);
 
             var template = $('#template-articulo').html();
+            // var templateTags = $('#template-tags').html();
             Mustache.parse(template);   // optional, speeds up future uses
+            // Mustache.parse(templateTags);   // optional, speeds up future uses
            
-            $("#articulo").html(''); 
+             $("#articulo").html(''); 
+            //  $("#tags").html(''); 
 
             let arrayMustache = [];
-            for (i = 0; i < 10; i++) {
+            for (i in data) {
                 let obj = data[i];
+                // let tags = data[i].tagss;
                 obj.FechaStr = moment(new Date(obj.createdAt)).format('DD/MM/YYYY');
+                obj.tags = data[i].tags;
+                obj.body = data[i].body.substring(0, 100)+"...";
                 arrayMustache.push(Mustache.render(template, obj));
+                // for (indece in data[i].tags){
+                //         arrayMustache.push(Mustache.render(template, tags));
+                //      }   
              // $("#articulo").append(
                     // ' <div class="row mb-5 articulos">' +
                     // '<div class="col-3">' +
